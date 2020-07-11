c61a02ab458a93978cad165acf382181be66bffb
Mateusz Grzeskowiak <mateusz.r.grzeskowiak@gmail.com>
2020-07-11 15:05:27 +0200

    done RandomStrategy

--- src/strategies/RandomStrategy.java
+++ src/strategies/RandomStrategy.java
@@ -1,20 +1,25 @@
 package strategies;
 
 import java.util.Random;
+import java.util.UUID;
 
 /**
  * Created by amen on 8/17/17.
  */
 public class RandomStrategy implements IInputStrategy {
+    Random random = new Random();
     @Override
     public int getInt() {
+        return random.nextInt();
     }
 
     @Override
     public String getString() {
+        return UUID.randomUUID().toString();
     }
 
     @Override
     public double getDouble() {
+        return random.nextDouble();
     }
 }
